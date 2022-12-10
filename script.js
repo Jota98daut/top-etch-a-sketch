95ba48dae0c289859e3776ed78d33a03a6d572d2
Joel D'Autilio <joeldautilio@gmail.com>
2022-12-10 17:19:46 -0300

    Add color and opacity selectors

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const WIDTH = 550;
 
-const grid = document.querySelector("#display");
-const sizeInput = document.querySelector("#size");
-const sizeSubmit = document.querySelector("#size-submit");
-const form = document.querySelector("form");
+const grid          = document.querySelector("#display");
+const sizeInput     = document.querySelector("#size");
+const sizeSubmit    = document.querySelector("#size-submit");
+const form          = document.querySelector("form");
+const opacityInput  = document.querySelector("#opacity");
+const opacityOutput = document.querySelector("#rangevalue");
+const colors        = document.querySelectorAll(".color");
+let cursorColor   = "black";
+let opacityDelta  = 0.5;
+
+opacityInput.onchange = _ => opacityDelta = parseFloat(opacityInput.value);
+opacityInput.addEventListener('change', _ => {
+  opacityDelta = parseFloat(opacityInput.value);
+  opacityOutput.innerText = opacityInput.value;
+})
 
 form.onsubmit = e => {
   e.preventDefault();
   generateGrid();
 };
+
+colors.forEach(color => {
+  color.style.background = color.id;
+  color.addEventListener('click', _ => cursorColor = color.id);
+});
 
 function generateGrid() {
   let size = sizeInput.value;
@@ -22,10 +38,15 @@ function generateGrid() {
       for(let j = 0; j < size; j++) {
         newCell = document.createElement("div");
         newCell.classList.add("cell");
-        newCell.setAttribute("data-x", i);
-        newCell.setAttribute("data-y", j);
         newCell.style.height = `${Math.floor(WIDTH / size)}px`;
+        newCell.style.opacity = 0;
         grid.appendChild(newCell);
+
+        newCell.addEventListener('mouseover', e => {
+          currentOpacity = parseFloat(e.target.style.opacity);
+          e.target.style.background = cursorColor;
+          e.target.style.opacity = Math.min(1, currentOpacity + opacityDelta);
+        });
       }
     }
   }
